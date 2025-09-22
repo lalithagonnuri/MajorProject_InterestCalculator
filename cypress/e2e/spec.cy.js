36fab953b741ce83836ff2f2462c14b3a8930aab
Uma <maheswariu403@gmail.com>
2025-09-22 14:02:29 +0530

    Modified

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -10,17 +10,9 @@ describe('EMI Calculator Automation Suite', () => {
   });
   it('Car Loan EMI Calculation - First Month Breakdown', function () {
     CarLoan.navigateToCarLoanPage() ;
-    CarLoan. fillDetails(this.data.amount, this.data.interestRate, this.data.loanTenure);
+    CarLoan.fillDetails(this.data.amount, this.data.interestRate, this.data.loanTenure);
     CarLoan.calculateEMI();
   });
-  it('Home Loan Emi Cal---Extract Yearly Data to Excel', function(){
-    HomeLoan.navigateToHomeLoanPage();
-    HomeLoan.fillDetails(this.homeData.price,this.homeData.margin,this.homeData.amount, this.homeData.interestRate, this.homeData.loanTenure,this.homeData.fee);
-    HomeLoan.getYearlyEMIData();
-    cy.fixture('year_on_year.json').then((data) => {
-      cy.task('writeExcel', data);
-    });
-  })
 });
 
 describe("UI Check",()=>{
